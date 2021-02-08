dc470152ed1f221c43e88ad233f2e548a6997292
tmtocb <kittz.esport@gmail.com>
2021-02-08 02:00:17 +0100

    add Product model validations with TDD

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Product, type: :model do
     it { is_expected.to have_db_column(:price) }
   end
 
+  describe '#validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:price) }
+  end
+
 end
